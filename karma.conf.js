6da90ce91705ea424c66263b688503a7f0482a82
mrjazz <denis.sheremetov@gmail.com>
2016-01-10 13:05:08 +0100

    refactoring assets and small redesign

--- karma.conf.js
+++ karma.conf.js
@@ -15,7 +15,7 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'core/*.js',
+      'js/*.js',
       'tests/*.test.js',
       'tests/*.test.js'
     ],
