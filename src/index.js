48138a965f96bfa8da81e836a5958d58577b4bce
mrjazz <denis.sheremetov@gmail.com>
2016-01-12 13:48:41 +0100

    rename GameUI in game-ui according to conventions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import GameUI from './components/GameUI';
+import GameUI from './components/game-ui';
 
 ReactDOM.render(<GameUI/>, document.getElementById('content'));
