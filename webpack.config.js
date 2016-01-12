d9ba60ad6b1bbc862224df5411b01ff891b2aa70
mrjazz <denis.sheremetov@gmail.com>
2016-01-12 13:42:46 +0100

    hot reload fix

--- webpack.config.js
+++ webpack.config.js
@@ -11,19 +11,17 @@ module.exports = {
     entry: [
         'webpack-dev-server/client?http://localhost:3001',
         'webpack/hot/only-dev-server',
-        './src/app.js'
+        './src/index'
     ],
     output: {
         path: path.join(__dirname, './dist'),
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/static/'
     },
     module: {
         loaders: [{
-            test: /\.jsx$|\.js$/,
-            loader: 'babel',
-            query: {
-                presets: ['react', 'es2015']
-            },
+            test: /\.jsx$|\.js$/,            
+            loaders: ['react-hot', 'babel'],
             include: path.join(__dirname, './src')
         }]
     },
