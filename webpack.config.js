12982fdcaf3e46184fa7a47b49937933d2ee654a
mrjazz <denis.sheremetov@gmail.com>
2016-01-12 13:30:58 +0100

    working tests with es6

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
-var path = require('path');
-var webpack = require('webpack');
+var path = require('path'),
+webpack = require('webpack');
 
 module.exports = {
-  devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './js/index'
-  ],
-  output: {
-    path: path.join(__dirname, 'dist'),
-    filename: 'bundle.js',
-    publicPath: '/static/'
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['react-hot', 'babel'],
-      include: path.join(__dirname, 'js')
-    }]
-  }
+    debug: true,
+    devtool: 'sourcemap',
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    entry: [
+        'webpack-dev-server/client?http://localhost:3001',
+        'webpack/hot/only-dev-server',
+        './src/app.js'
+    ],
+    output: {
+        path: path.join(__dirname, './dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.jsx$|\.js$/,
+            loader: 'babel',
+            query: {
+                presets: ['react', 'es2015']
+            },
+            include: path.join(__dirname, './src')
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    }
 };
